7a8d8c59ef20dabf044697b8332ecfc90d13cb17
wisdom014 <elijahakinniyi014@gmail.com>
2026-06-11 23:00:04 +0100

    navbar fix

--- src/components/contact.js
+++ src/components/contact.js
@@ -10,7 +10,7 @@ function ContactSection() {
 
         <p>
           Please get in touch and our expert support team will answer all
-          your questions.
+          your questions
         </p>
       </div>
 
